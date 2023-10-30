1a5a2cb499e66c42b50e15f92a941033424d267b
NAVEENK352 <50852362+NAVEENK352@users.noreply.github.com>
2023-10-30 13:08:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import os
+import requests
+import uvicorn
 
 app = FastAPI()
+BOT_KEY = os.getenv("TELEGRAM_BOT_KEY")
+BOT_URL = f"https://api.telegram.org/bot{BOT_KEY}"
 
-@app.get("/")
-def hello():
-    return {"message": "Hello, New world!"}
+async def get_webhook_info():
+    message_url = f"{BOT_URL}/getWebhookInfo"
+    return await requests.get(message_url).json()
+    
+@app.get("/get_webhook/")
+async def get_webhook():
+    return await get_webhook_info()
+
+if __name__ == '__main__':
+    uvicorn.run(app)
