c228b0fec20004361b8d5a313435cd3dde0266fb
NAVEENK352 <50852362+NAVEENK352@users.noreply.github.com>
2023-10-30 14:08:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,51 @@ import os
 import aiohttp
 
 app = FastAPI()
-BOT_KEY = os.getenv("TELEGRAM_BOT_KEY")
+BOT_KEY = os.environ["TELEGRAM_BOT_KEY"] # get the bot token from environment variable
 BOT_URL = f"https://api.telegram.org/bot{BOT_KEY}"
 
 async def get_webhook_info():
+    """Get the current webhook information from Telegram API"""
     message_url = f"{BOT_URL}/getWebhookInfo"
     async with aiohttp.ClientSession() as session:
-        async with session.get(message_url) as response:
-            data = await response.json()
-            return data
+        try:
+            async with session.get(message_url) as response:
+                webhook_info = await response.json()
+                return webhook_info
+        except Exception as e:
+            print(e)
+            return None
 
 @app.get("/set_webhook")
 async def url_setter():
-    PROG_URL = os.getenv("DETA_SPACE_APP_HOSTNAME")
+    """Set the webhook URL for Telegram API"""
+    PROG_URL = os.environ["DETA_SPACE_APP_HOSTNAME"] # get the server URL from environment variable
     set_url = f"{BOT_URL}/setWebHook?url=https://{PROG_URL}/open"
     async with aiohttp.ClientSession() as session:
-        async with session.get(set_url) as response:
-            resp = await response.json()
-            return resp
+        try:
+            async with session.get(set_url) as response:
+                webhook_result = await response.json()
+                return webhook_result
+        except Exception as e:
+            print(e)
+            return None
 
 @app.get("/get_webhook/")
 async def get_webhook():
+    """Return the current webhook information"""
     return await get_webhook_info()
 
 @app.post("/open")
 async def http_handler(request: Request):
-    incoming_data = await request.json()
-    #prompt = incoming_data["message"]["text"]
-    prompt = incoming_data.get("message", {}).get("text", "No text found")
-    chat_id = incoming_data["message"]["chat"]["id"]
+    """Handle the incoming messages from Telegram"""
+    try:
+        incoming_data = await request.json() # parse the request data as JSON
+        prompt = incoming_data.get("message", {}).get("text", "No text found") # get the text from the message
+        chat_id = incoming_data["message"]["chat"]["id"] # get the chat id from the message
+    except Exception as e:
+        print(e)
+        return None
+
     if "message" not in incoming_data:
         print(incoming_data)
         return send_error(None, "Unknown error, lol, handling coming soon")
@@ -42,12 +58,16 @@ async def http_handler(request: Request):
         )
         payload = {"text": response_text, "chat_id": chat_id}
         message_url = f"{BOT_URL}/sendMessage"
-        requests.post(message_url, json=payload).json()
-        return
-    
+        async with aiohttp.ClientSession() as session: # use aiohttp instead of requests
+            try:
+                async with session.post(message_url, json=payload) as response: # use post method to send message
+                    resp = await response.json() # get the response data as JSON
+                    return resp
+            except Exception as e:
+                print(e)
+                return None
     
     return     
-
 
 if __name__ == '__main__':
     uvicorn.run(app)
