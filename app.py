b80c6c66aee8201a555fcb164709ed5e057a4296
NAVEENK352 <50852362+NAVEENK352@users.noreply.github.com>
2023-10-30 13:29:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 import os
-import requests
-import uvicorn
+import aiohttp
 
 app = FastAPI()
 BOT_KEY = os.getenv("TELEGRAM_BOT_KEY")
@@ -9,11 +8,15 @@ BOT_URL = f"https://api.telegram.org/bot{BOT_KEY}"
 
 async def get_webhook_info():
     message_url = f"{BOT_URL}/getWebhookInfo"
-    return await requests.get(message_url).json()
+    async with aiohttp.ClientSession() as session:
+        async with session.get(message_url) as response:
+            data = await response.json()
+            return data
     
 @app.get("/get_webhook/")
 async def get_webhook():
     return await get_webhook_info()
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
